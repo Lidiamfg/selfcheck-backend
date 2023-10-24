0dd155d8fdbc210fdd9934e055c17412b58a82e4
Tiago Gil <talmeidagil@hotmail.com>
2023-10-24 13:02:47 +0100

    some changes

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ const app = express();
 require("./config")(app);
 
 // 👇 Start handling routes here
+
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
-const authRoutes = require('./routes/auth.routes')
-app.use('/auth', authRoutes)
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
