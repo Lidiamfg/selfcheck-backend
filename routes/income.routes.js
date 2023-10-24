61dd70cb57083e17229701ccea04e3e68ce8e55a
Tiago Gil <talmeidagil@hotmail.com>
2023-10-24 16:57:21 +0100

    we managed the relationships

--- routes/income.routes.js
+++ routes/income.routes.js
@@ -1,6 +1,7 @@
 const { isAuthenticated } = require("../middlewares/routeGuard.middleware");
 
 const router = require("express").Router();
+const User = require("../models/User.model")
 
 const Income = require("../models/Income.model");
 
@@ -12,13 +13,25 @@ router.get("/", isAuthenticated, (req, res, next) => {
   res.json('All good in here')
 }) */
 
-router.post("/", /* isAuthenticated, */ async (req, res, next) => {
+router.post("/", isAuthenticated, async (req, res) => {
   try {
-    const newIncome = await Income.create(req.body);
+    const userId = req.body.user;
+    const incomeData = { ...req.body, user: userId };
+    console.log("UserID:", userId);
+    console.log("Income Data:", incomeData);
+
+    const newIncome = await Income.create(incomeData);
+    console.log("New Income:", newIncome);
+
+    await User.findByIdAndUpdate(userId, { $push: { income: newIncome._id } });
+
+    const updatedUser = await User.findById(userId);
+    console.log("Updated User:", updatedUser);
+
     res.status(201).json({ income: newIncome });
   } catch (error) {
     console.log(error);
-    res.status(500).json({error})
+    res.status(500).json({ error });
   }
 });
 
