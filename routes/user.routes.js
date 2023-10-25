6c96255d04db9d35f50fc04e7c9c223776e99957
Tiago Gil <talmeidagil@hotmail.com>
2023-10-25 17:35:46 +0100

    changed user route to id instead of username

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,15 +5,20 @@ const User = require("../models/User.model");
 /* const Income = require("../models/Income.model");
 const Expense = require("../models/Expense.model"); */
 
-router.get('/:username', isAuthenticated, async (req, res) => {
-  const { username } = req.params
+router.get("/:userId", isAuthenticated, async (req, res) => {
+  const { userId } = req.params;
 
   try {
-    const oneUser = await User.findOne({ username: username }).populate('income').populate('expense')
-    res.status(200).json({ user: oneUser })
+    const oneUser = await User.findById(userId)
+      .populate("income")
+      .populate("expense");
+    const userCopy = oneUser._doc;
+    delete userCopy.passwordHash;
+    res.status(200).json({ user: userCopy });
+    console.log(userCopy)
   } catch (error) {
-    res.status(500).json({ error })
+    res.status(500).json({ error });
   }
-})
+});
 
 module.exports = router;
