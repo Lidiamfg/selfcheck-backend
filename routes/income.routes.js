9e70c4bf0fe7744f280913d9ba952f5f7f91742a
Tiago Gil <talmeidagil@hotmail.com>
2023-10-25 10:16:16 +0100

    income route done

--- routes/income.routes.js
+++ routes/income.routes.js
@@ -1,7 +1,7 @@
 const { isAuthenticated } = require("../middlewares/routeGuard.middleware");
 
 const router = require("express").Router();
-const User = require("../models/User.model")
+const User = require("../models/User.model");
 
 const Income = require("../models/Income.model");
 
@@ -13,34 +13,52 @@ router.get("/", isAuthenticated, (req, res, next) => {
   res.json('All good in here')
 }) */
 
-router.post("/", isAuthenticated, async (req, res) => {
-  try {
-    const userId = req.body.user;
-    const incomeData = { ...req.body, user: userId };
-    console.log("UserID:", userId);
-    console.log("Income Data:", incomeData);
-
-    const newIncome = await Income.create(incomeData);
-    console.log("New Income:", newIncome);
-
-    await User.findByIdAndUpdate(userId, { $push: { income: newIncome._id } });
-
-    const updatedUser = await User.findById(userId);
-    console.log("Updated User:", updatedUser);
-
-    res.status(201).json({ income: newIncome });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error });
+router.post(
+  "/",
+  /* isAuthenticated, */ async (req, res) => {
+    try {
+      const userId = req.body.user;
+      const incomeData = { ...req.body, user: userId };
+      const newIncome = await Income.create(incomeData);
+      await User.findByIdAndUpdate(userId, {
+        $push: { income: newIncome._id },
+      });
+      res.status(201).json({ income: newIncome });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error });
+    }
   }
-});
+);
 
-router.put("/:id", isAuthenticated, (req, res, next) => {
-  res.json("All good in here");
-});
+router.put(
+  "/:incomeId",
+  /* isAuthenticated, */ async (req, res) => {
+    const { incomeId } = req.params;
+    try {
+      const updatedIncome = await Income.findByIdAndUpdate(incomeId, req.body, {
+        new: true,
+      });
+      res.status(200).json({ income: updatedIncome });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error });
+    }
+  }
+);
 
-router.delete("/:id", isAuthenticated, (req, res, next) => {
-  res.json("All good in here");
-});
+router.delete(
+  "/:incomeId",
+  /* isAuthenticated, */ async (req, res) => {
+    const { incomeId } = req.params;
+    try {
+      await Income.findByIdAndDelete(incomeId);
+      res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error });
+    }
+  }
+);
 
 module.exports = router;
