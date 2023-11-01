2fc0bbc84f577116e55bab8adbc0d72d4782fdc1
Krystian Jakubczyk <krystianjnl@gmail.com>
2023-11-01 17:22:10 +0100

    Deleting user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -29,5 +29,18 @@ router.get("/:userId",  isAuthenticated,  async (req, res) => {
     res.status(500).json({ error });
   }
 });
+router.delete(
+  "/:userId",
+  isAuthenticated, async (req, res) => {
+    const { userId } = req.params;
+    try {
+      await User.findByIdAndDelete(userId);
+      res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error });
+    }
+  }
+);
 
 module.exports = router;
